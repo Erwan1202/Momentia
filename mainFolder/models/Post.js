6380053d70cb1beda4cbe19e3859cbb55b2f7682
Lord Erwan <erwan.a.marechal@gmail.com>
2025-02-19 16:40:08 +0100

    Refactor Post model to use class-based structure with CRUD methods for post management

--- mainFolder/models/Post.js
+++ mainFolder/models/Post.js
@@ -2,19 +2,58 @@ const { DataTypes } = require('sequelize');
 const db = require('../config/db');
 const User = require('./User');
 
-const Post = db.define('Post', {
-    id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
-    user_id: { type: DataTypes.BIGINT, allowNull: false },
-    caption: { type: DataTypes.TEXT, allowNull: true },
-    image_url: { type: DataTypes.TEXT, allowNull: false },
-    location: { type: DataTypes.STRING(100), allowNull: true },
-    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-}, {
-    timestamps: false,
-    tableName: 'posts'
-});
+class Post{
+    static async getAllPost(){
+        const result = await db.query('SELECT * FROM posts');
+        return result.rows;
+    }
 
-// Relation : Un Post appartient à un User
-Post.belongsTo(User, { foreignKey: 'user_id' });
+    static async getPostById(id){
+        const result = await db.query('SELECT * FROM posts WHERE id = $1', [id]);
+        return result.rows[0];
+    }
+
+    static async createPost(data){
+        const result = await db.query('INSERT INTO posts (title, content, user_id) VALUES ($1, $2, $3) RETURNING *', [data.title, data.content, data.user_id]);
+        return result.rows[0];
+    }
+
+    static async updatePost(id, data){
+        const result = await db.query('UPDATE posts SET title = $1, content = $2 WHERE id = $3 RETURNING *', [data.title, data.content, id]);
+        return result.rows[0];
+    }
+
+    static async deletePost(id){
+        const result = await db.query('DELETE FROM posts WHERE id = $1', [id]);
+        return result;
+    }
+
+    static async patchPost(id, data){
+        const key = Object.keys(data);
+        const setClause = key.map((item, index) => {
+            return `${item} = $${index + 1}`;
+        }
+        ).join(', ');
+        const values = Object.values(data);
+        const query = {
+            text: `UPDATE posts SET ${setClause} WHERE id = $${key.length + 1} RETURNING *`,
+            values: [...values, id]
+        }
+        const result = await db.query(query);
+        return result.rows[0];
+    }
+
+    static async getPostByUser(id){
+        const result = await db.query('SELECT * FROM posts WHERE user_id = $1', [id]);
+        return result.rows;
+    }
+
+    static async getPostByUserEmail(email){
+        const user = await User.getUserByEmail(email);
+        const result = await db.query('SELECT * FROM posts WHERE user_id = $1', [user.id]);
+        return result.rows;
+    }
+
+}
 
 module.exports = Post;
