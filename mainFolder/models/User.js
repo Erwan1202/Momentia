ee6bb30354ff7c91101e69ed5bb0f368906c03c7
Lord Erwan <erwan.a.marechal@gmail.com>
2025-02-19 16:38:44 +0100

    Refactor User model to use class-based structure with CRUD methods for user management

--- mainFolder/models/User.js
+++ mainFolder/models/User.js
@@ -1,17 +1,54 @@
 const { DataTypes } = require('sequelize');
 const db = require('../config/db');
 
-const User = db.define('User', {
-    id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
-    username: { type: DataTypes.STRING(50), allowNull: false, unique: true },
-    email: { type: DataTypes.STRING(100), allowNull: false, unique: true },
-    password_hash: { type: DataTypes.TEXT, allowNull: false },
-    avatar_url: { type: DataTypes.TEXT, allowNull: true },
-    bio: { type: DataTypes.TEXT, allowNull: true },
-    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
-}, {
-    timestamps: false,
-    tableName: 'users'
-});
+class User{
+    static async getAllUser(){7
+        const result = await db.query('SELECT * FROM users');
+        return result.rows;
+    }
+
+    static async getUserById(id){
+        const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+        return result.rows[0];
+    }
+
+    static async getUserByEmail(email){
+        const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        return result.rows[0];
+    }
+
+    static async createUser(data){
+        const result = await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [data.name, data.email, data.password]);
+        return result.rows[0];
+    }
+
+    static async updateUser(id, data){
+        const result = await db.query('UPDATE users SET name = $1, email = $2, password = $3 WHERE id = $4 RETURNING *', [data.name, data.email, data.password, id]);
+        return result.rows[0];
+    }
+
+    static async deleteUser(id){
+        const result = await db.query('DELETE FROM users WHERE id = $1', [id]);
+        return result;
+    }
+
+    static async patchUser(id, data){
+        const key = Object.keys(data);
+        const setClause = key.map((item, index) => {
+            return `${item} = $${index + 1}`;
+        }
+        ).join(', ');
+        const values = Object.values(data);
+        const query = {
+            text: `UPDATE users SET ${setClause} WHERE id = $${key.length + 1} RETURNING *`,
+            values: [...values, id]
+        }
+        const result = await db.query(query);
+        return result.rows[0];
+    }
+
+
+
+}
 
 module.exports = User;
