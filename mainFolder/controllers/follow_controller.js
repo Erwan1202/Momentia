afd220af71a609fdf9d0bfc6261f39b0730119d1
Lord Erwan <erwan.a.marechal@gmail.com>
2025-03-12 17:39:01 +0100

    Implement follow functionality: add routes and controller methods for creating, deleting, and retrieving follows by user and follower

--- mainFolder/controllers/follow_controller.js
+++ mainFolder/controllers/follow_controller.js
@@ -1,29 +1,112 @@
+const follow = require('../models/follow');
 const db = require('../config/db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const Follow = require('../models/follow');
 
 
-// récupérer tous les follows d'un user
-exports.getUserFollow = (req, res) => {
-    const { id } = req.params;
-    const query = 'SELECT * FROM follows WHERE user_id = ?';
-    db.query(query, [id], (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json(result);
-    });
-};
+//creation follow
+exports.createFollow = (req, res, next) => {
+    try {
+        const follow = new Follow.createFollow({
+            user_id: req.body.user_id,
+            follower_id: req.body.follower_id});
+            res.status(201).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
 
-// récupérer tous les followers d'un user
-exports.getUserFollower = (req, res) => {
-    const { id } = req.params;
-    const query = 'SELECT * FROM follows WHERE follower_id = ?';
-    db.query(query, [id], (err, result) => {
-        if (err) {
-            return res.status(500).json({ error: err.message });
-        }
-        res.json(result);
-    });
-};
+//suppression follow
+exports.deleteFollow = (req, res, next) => {
+    try {
+        const follow = Follow.deleteFollow(req.params.id);
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
 
+//recuperation follow par user
+exports.getFollowByUser = (req, res, next) => {
+    try {
+        const follow = Follow.getFollowByUser(req.params.id);
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
+
+//recuperation follow par follower
+exports.getFollowByFollower = (req, res, next) => {
+    try {
+        const follow = Follow.getFollowByFollower(req.params.id);
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
+
+//recuperation follow par user et follower
+exports.getFollowByUserAndFollower = (req, res, next) => {
+    try {
+        const follow = Follow.getFollowByUserAndFollower(req.params.user_id, req.params.follower_id);
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
+
+//recuperation follow par user email
+exports.getFollowByUserEmail = (req, res, next) => {
+    try {
+        const follow = Follow.getFollowByUserEmail(req.params.email);
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
+
+//recuperation follow par follower email
+exports.getFollowByFollowerEmail = (req, res, next) => {
+    try {
+        const follow = Follow.getFollowByFollowerEmail(req.params.email);
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
+
+//recuperation de tous les follow
+exports.getAllFollow = (req, res, next) => {
+    try {
+        const follow = Follow.getAllFollow();
+        res.status(200).json(follow);
+    }
+    catch (error) {
+        res.status(500).json({
+            error: error
+        });
+    }
+}
